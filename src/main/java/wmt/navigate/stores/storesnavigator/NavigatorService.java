f673e5498bdcc07027a58ac9aadef4b461c629e7
sudikhya <sudikhyagoswami@gmail.com>
2019-05-02 23:20:24 +0530

    making venue optional

--- src/main/java/wmt/navigate/stores/storesnavigator/NavigatorService.java
+++ src/main/java/wmt/navigate/stores/storesnavigator/NavigatorService.java
@@ -7,6 +7,7 @@ import javax.xml.bind.JAXBException;
 
 import org.json.JSONException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Required;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -19,8 +20,8 @@ public class NavigatorService {
 
 	@GetMapping("/navigationdata")
 	public List<NavigationDetails> getNavigationDetails(@RequestParam("api_key") String apiKey,
-			@RequestParam("venue_name") String venueName) throws JAXBException, IOException, JSONException {
-		if (venueName.equals(null)) {
+			@RequestParam(value = "venue_name", required = false) String venueName) throws JAXBException, IOException, JSONException {
+		if (venueName == null) {
 			return navigatorManager.getNavigationDetails(apiKey);
 		} else {
 			return navigatorManager.getNavigationDetailsForVenue(apiKey, venueName);
